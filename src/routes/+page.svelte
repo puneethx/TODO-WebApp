041445a84b8c4b1003a6d1b87832baf8056298c3
puneethx <puneethreddyt2004@gmail.com>
2023-04-02 23:16:54 +0530

    AddText

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,22 @@
         {task:"Get Tickets", isComplete: false, createdAt :"2021-08-06"},
         {task:"Get Bread", isComplete: true, createdAt :"2021-07-06"},
         ]
+
+        let todo = {
+            task:"",
+            isComplete:false,
+            createdAt:new Date(),
+        };
+        const addTodo = () => {
+            todos = [...todos,todo];
+        }
+
+        $: console.table(todos);
 </script>
+
+<input type="text" bind:value={todo.task} />
+<button on:click={addTodo}>Add</button>
 <ol>
-    <h1>TODO</h1>
     {#each todos as todo}
     <li class:complete={todo.isComplete}>{todo.task}</li>
     {/each}
