994f25a42fcaedfb8a493b0830aaff92a65eeccd
puneethx <puneethreddyt2004@gmail.com>
2023-04-02 22:39:34 +0530

    LineThrough

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script>
-    let Todos = ["Hii","I am Puneeth","This is a project"]
+    let todos = [
+        {task:"Get books", isComplete: false, createdAt :"2021-08-06"},
+        {task:"Get Tickets", isComplete: false, createdAt :"2021-08-06"},
+        {task:"Get Bread", isComplete: true, createdAt :"2021-07-06"},
+        ]
 </script>
 <ol>
     <h1>TODO</h1>
-    {#each Todos as Todo}
-    <li>{Todo}</li>
+    {#each todos as todo}
+    <li class:complete={todo.isComplete}>{todo.task}</li>
     {/each}
 </ol>
+
+<style>
+    .complete{
+        text-decoration: line-through;
+    }
+</style>
